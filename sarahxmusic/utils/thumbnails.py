019dfa24ac7953cfd34ca0d6a5c8f5d950d03ddf
SmartGirl15 <92010030+SmartGirl15@users.noreply.github.com>
2024-07-18 23:07:25 +0530

    Update thumbnails.py

--- sarahxmusic/utils/thumbnails.py
+++ sarahxmusic/utils/thumbnails.py
@@ -1,138 +1,204 @@
-import asyncio
 import os
-import random
 import re
 import textwrap
+import random
+
 import aiofiles
 import aiohttp
-from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
-                 ImageFont, ImageOps)
-from youtubesearchpython.__future__ import VideosSearch
 import numpy as np
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
-def make_col():
-    return (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+from PIL import Image, ImageChops, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+import PIL
+from youtubesearchpython.__future__ import VideosSearch
+
+from config import YOUTUBE_IMG_URL, MUSIC_BOT_NAME
+from sarahxmusic import app
+
+
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
     heightRatio = maxHeight / image.size[1]
     newWidth = int(widthRatio * image.size[0])
     newHeight = int(heightRatio * image.size[1])
-    newImage = image.resize((newWidth, newHeight))
+    newImage = image.resize((newWidth, newHeight))    
     return newImage
 
-def truncate(text):
-    list = text.split(" ")
-    text1 = ""
-    text2 = ""    
-    for i in list:
-        if len(text1) + len(i) < 30:        
-            text1 += " " + i
-        elif len(text2) + len(i) < 30:       
-            text2 += " " + i
 
-    text1 = text1.strip()
-    text2 = text2.strip()     
-    return [text1,text2]
+def add_corners(im):
+    bigsize = (im.size[0] * 5, im.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    size = [(0,100), bigsize]
+    ImageDraw.Draw(mask).rectangle(size, fill=255)
+    mask = mask.resize(im.size, Image.LANCZOS)
+    mask = ImageChops.darker(mask, im.split()[-1])
+    im.putalpha(mask)
 
-async def get_thumb(videoid, user_id):
+
+
+async def gen_thumb(videoid, user_id):
+    if os.path.isfile(f"cache/{videoid}_{user_id}.png"):
+        return f"cache/{videoid}_{user_id}.png"
+    url = f"https://www.youtube.com/watch?v={videoid}"
     try:
-        if os.path.isfile(f"cache/{videoid}_{user_id}.png"):
-            return f"cache/{videoid}_{user_id}.png"
-        url = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
+                
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    await f.write(await resp.read())
+                    await f.close()       
+                
+        
+        youtube = Image.open(f"cache/thumb{videoid}.png")        
+        image1 = changeImageSize(1280, 720, youtube)
+        image2 = image1.convert("RGBA")
+        #background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        enhancer = ImageEnhance.Brightness(image2)
+        background = enhancer.enhance(1)
+
+        bg = Image.open(f"assets/rose2.png")
+        #bg = random.choice(BACKGROUND)
+        image3 = changeImageSize(1280, 720, bg)
+        image5 = image3.convert("RGBA")
+        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
+
+        Xcenter = youtube.width / 2
+        Ycenter = youtube.height / 2
+        x1 = Xcenter - 640
+        y1 = Ycenter - 725
+        x2 = Xcenter + 640
+        y2 = Ycenter + 555
+        logo = youtube.crop((x1, y1, x2, y2))
+        logo.thumbnail((342, 342), Image.LANCZOS)
+        logo.save(f"cache/chop{videoid}.png")
+        if not os.path.isfile(f"cache/cropped{videoid}.png"):
+            im = Image.open(f"cache/chop{videoid}.png").convert("RGBA")
+            add_corners(im)
+            im.save(f"cache/cropped{videoid}.png")
+
+        crop_img = Image.open(f"cache/cropped{videoid}.png")
+        logo = crop_img.convert("RGBA")
+        logo.thumbnail((400, 400), Image.LANCZOS)
+        #logo = ImageOps.expand(logo, border=10, fill="orange")
+        background = Image.open(f"cache/temp{videoid}.png")
+        #print("Reached abc line")
+        #abc = await process_user_profile(app, user_id)
+        #print("Crossed abc line")
+        background.paste(logo, (50, 365), mask=logo)
+        #print("line 128")
+        async def user_profile(app, user_id):
+            try:
+                wxyz = await app.get_chat_photos(user_id)
+                wxy = await app.download_media(wxyz[0]['file_id'], file_name=f'{user_id}.jpg')
+            except:
+                hehe = await app.get_chat_photos(app.id)
+                wxy = await app.download_media(hehe[0]['file_id'], file_name=f'{app.id}.jpg')
+
+            xy = Image.open(wxy)
+            a = Image.new('L', [640, 640], 0)
+            b = ImageDraw.Draw(a)
+            b.pieslice([(0, 0), (640, 640)], 0, 360, fill=255, outline="white")
+            c = np.array(xy)
+            d = np.array(a)
+            e = np.dstack((c, d))
+            f = Image.fromarray(e)
+            x = f.resize((245, 245))
+    
+            return x
+        #abc = await user_profile(app, user_id)
+        #background.paste(abc, (947, 118), mask=abc)
+        #print("Success!")
+
+        draw = ImageDraw.Draw(background)
+        font = ImageFont.truetype("assets/Hungline.ttf", 45)
+        font2 = ImageFont.truetype("assets/Hawaii.otf", 150)
+        arial = ImageFont.truetype("assets/font2.ttf", 30)
+        name_font = ImageFont.truetype("assets/font2.ttf", 35)
+        para = textwrap.wrap(title, width=32)
+        j = 0
+        draw.text(
+            (935, 655), 
+            f"with {MUSIC_BOT_NAME}", 
+            fill="black",
+            stroke_width=1,
+            stroke_fill="yellow", 
+            font=name_font
+        )
+        draw.text(
+            (945, 115),
+            "Enjoyie",
+            fill="white",
+            stroke_width=2,
+            stroke_fill="black",
+            font=font2,
+        )
+        draw.text(
+            (1040, 270),
+            "the",
+            fill="white",
+            stroke_width=2,
+            stroke_fill="black",
+            font=font2,
+        )
+        draw.text(
+            (1015, 360),
+            "song!",
+            fill="white",
+            stroke_width=2,
+            stroke_fill="black",
+            font=font2,
+        )
+        for line in para:
+            if j == 1:
+                j += 1
+                draw.text(
+                    (100, 65),
+                    f"{line}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="blue",
+                    font=font,
+                )
+            if j == 0:
+                j += 1
+                draw.text(
+                    (150, 105),
+                    f"{line}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="blue",
+                    font=font,
+                )
+        
         try:
-            results = VideosSearch(url, limit=1)
-            for result in (await results.next())["result"]:
-                try:
-                    title = result["title"]
-                    title = re.sub("\W+", " ", title)
-                    title = title.title()
-                except:
-                    title = "Unsupported Title"
-                try:
-                    duration = result["duration"]
-                except:
-                    duration = "Unknown"
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                try:
-                    views = result["viewCount"]["short"]
-                except:
-                    views = "Unknown Views"
-                try:
-                    channel = result["channel"]["name"]
-                except:
-                    channel = "Unknown Channel"
-
-            async with aiohttp.ClientSession() as session:
-                #async with session.get(f"http://img.youtube.com/vi/{videoid}/maxresdefault.jpg") as resp:
-                async with session.get(thumbnail) as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
-                        await f.write(await resp.read())
-                        await f.close()
-
-            youtube = Image.open(f"cache/thumb{videoid}.jpg")
-            image1 = changeImageSize(1280, 720, youtube)
-            image2 = image1.convert("RGBA")
-            background = image2.filter(filter=ImageFilter.BoxBlur(30))
-            enhancer = ImageEnhance.Brightness(background)
-            background = enhancer.enhance(0.6)
-            image2 = background
-                                                                                            
-            circle = Image.open(f"sarahxmusic/assets/circle.png")
-
-            # changing circle color
-            im = circle
-            im = im.convert('RGBA')
-            color = make_col()
-
-            data = np.array(im)
-            red, green, blue, alpha = data.T
-
-            white_areas = (red == 255) & (blue == 255) & (green == 255)
-            data[..., :-1][white_areas.T] = color
-
-            im2 = Image.fromarray(data)
-            circle = im2
-            # done
-
-            image3 = image1.crop((280,0,1000,720))
-            lum_img = Image.new('L', [720,720] , 0)
-            draw = ImageDraw.Draw(lum_img)
-            draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
-            img_arr = np.array(image3)
-            lum_img_arr = np.array(lum_img)
-            final_img_arr = np.dstack((img_arr,lum_img_arr))
-            image3 = Image.fromarray(final_img_arr)
-            image3 = image3.resize((600,600))
-            
-
-            image2.paste(image3, (50,70), mask = image3)
-            image2.paste(circle, (0,0), mask = circle)
-
-            # fonts
-            font1 = ImageFont.truetype('sarahxmusic/assets/font.ttf', 30)
-            font2 = ImageFont.truetype('sarahxmusic/assets/font2.ttf', 70)
-            font3 = ImageFont.truetype('sarahxmusic/assets/font2.ttf', 40)
-            font4 = ImageFont.truetype('sarahxmusic/assets/font2.ttf', 35)
-
-            image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), f"{MUSIC_BOT_NAME}", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
-
-            # title
-            title1 = truncate(title)
-            image4.text((670, 300), text=title1[0], fill="white", stroke_width=1, stroke_fill="white",font = font3, align ="left") 
-            image4.text((670, 350), text=title1[1], fill="white", stroke_width=1, stroke_fill="white", font = font3, align ="left") 
-
-
-          
-            image2 = ImageOps.expand(image2,border=20,fill=make_col())
-            image2 = image2.convert('RGB')
-            image2.save(f"cache/{videoid}.png")
-            file = f"cache/{videoid}.png"
-            return file        
+            os.remove(f"cache/thumb{videoid}.png")
+        except:
+            pass
+        background.save(f"cache/{videoid}_{user_id}.png")
+        return f"cache/{videoid}_{user_id}.png"
     except Exception as e:
         print(e)
-        return YOUTUBE_IMG_URL
+        return (YOUTUBE_IMG_URL)
+    
